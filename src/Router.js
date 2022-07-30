ff4449c97d2689f83d5677dd71415b8fc37cac7d
oguz yurken <oguz.yurken@gmail.com>
2022-07-31 02:07:16 +0300

    Add Redux | Add Wrapper | Add Login page

--- src/Router.js
+++ src/Router.js
@@ -4,40 +4,61 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Detail from './pages/Detail';
 import Products from './pages/Products';
 import Login from './pages/Login';
-
+import {useDispatch, useSelector} from 'react-redux';
+import Loading from './components/Loading';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 const Stack = createNativeStackNavigator();
 const Router = () => {
+  const userSession = useSelector(s => s.user);
+  const isAuthLoading = useSelector(s => s.isAuthLoading);
+  const dispatch = useDispatch();
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="LoginPage"
-          component={Login}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="ProductsPage"
-          component={Products}
-          options={{
-            title: 'Dukkan',
-            headerStyle: {backgroundColor: '#90caf9'},
-            headerTitleStyle: {color: 'white'},
-            headerTitleAlign: 'center',
-          }}
-        />
-        <Stack.Screen
-          name="DetailPage"
-          component={Detail}
-          options={{
-            title: 'Detay',
-            headerStyle: {backgroundColor: '#90caf9'},
-            headerTitleStyle: {color: 'white'},
-            headerTitleAlign: 'center',
-          }}
-        />
-      </Stack.Navigator>
+      {isAuthLoading ? (
+        <Loading />
+      ) : !userSession ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="LoginPage"
+            component={Login}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="ProductsPage"
+            component={Products}
+            options={{
+              title: 'Dukkan',
+              headerStyle: {backgroundColor: '#90caf9'},
+              headerTitleStyle: {color: 'white'},
+              headerTitleAlign: 'center',
+              headerRight: () => (
+                <Icon
+                  name="logout"
+                  size={30}
+                  color="white"
+                  onPress={() => dispatch({type: 'REMOVE_USER'})}
+                />
+              ),
+            }}
+          />
+          <Stack.Screen
+            name="DetailPage"
+            component={Detail}
+            options={{
+              title: 'Detay',
+              headerStyle: {backgroundColor: '#90caf9'},
+              headerTitleStyle: {color: 'white'},
+              headerTitleAlign: 'center',
+            }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -7,8 +7,11 @@ import {Formik} from 'formik';
 import usePost from '../../hooks/usePost/usePost';
 import Config from 'react-native-config';
 
-const Login = ({navigation}) => {
+import {useDispatch} from 'react-redux';
+
+const Login = () => {
   const {data, post, loading, error} = usePost();
+  const dispatch = useDispatch();
 
   const handeLogin = values => {
     post(Config.API_AUTH_URL + '/login', values);
@@ -22,7 +25,7 @@ const Login = ({navigation}) => {
     if (data === 'Error') {
       Alert.alert('Dukkan', 'Kullanici Bulunamadi!');
     } else {
-      navigation.navigate('ProductsPage');
+      dispatch({type: 'SET_USER', payload: {user}});
     }
   }
 
@@ -62,3 +65,26 @@ const Login = ({navigation}) => {
 };
 
 export default Login;
+
+const user = {
+  address: {
+    geolocation: {
+      lat: '-37.3159',
+      long: '81.1496',
+    },
+    city: 'kilcoole',
+    street: 'new road',
+    number: 7682,
+    zipcode: '12926-3874',
+  },
+  id: 1,
+  email: 'john@gmail.com',
+  username: 'johnd',
+  password: 'm38rmF$',
+  name: {
+    firstname: 'john',
+    lastname: 'doe',
+  },
+  phone: '1-570-236-7033',
+  __v: 0,
+};
